3112dcfc475d97fc96f4265a972cdbbee003433c
M Farkas-Dyck <strake888@gmail.com>
2020-12-23 15:08:48 -0800

    modify atomically

--- situ.sh
+++ situ.sh
@@ -4,7 +4,7 @@ n_fail=0
 
 while read -r file; do
 	if ! test -e "$file"; then echo "no such file: $file" 1>&2; continue; fi
-	if tmp=$(mktemp /tmp/situ.XXXXXXXXXXXX) && "$@" <"$file" >"$tmp" && cat <"$tmp" >"$file" && rm "$tmp"; then echo "$file"
+	if tmp=$(mktemp "$(dirname "$file")/.situ.XXXXXXXXXXXX") && "$@" <"$file" >"$tmp" && mv "$tmp" "$file"; then echo "$file"
 	else
 		echo "failed to modify $file" 1>&2
 		n_fail=$(($n_fail + 1))
